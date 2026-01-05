7c910e73d8e071dd4a25748388af196df725b29d
Sander in 't Hout <sander@inthout.me>
2026-01-05 20:57:06 +0100

    Preload Google Fonts to prevent layout jumps

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:algolab/algo_lab_app.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 
 GetIt getIt = GetIt.instance;
@@ -8,6 +9,12 @@ GetIt getIt = GetIt.instance;
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   getIt.registerSingleton(await PackageInfo.fromPlatform());
+
+  // Preload fonts to avoid layout jumps.
+  await GoogleFonts.pendingFonts([
+    GoogleFonts.sueEllenFrancisco(),
+    GoogleFonts.enriqueta(),
+  ]);
 
   runApp(const AlgoLabApp());
 }
